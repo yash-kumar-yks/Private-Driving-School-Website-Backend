5ada729a1ba708182800946a09df163659f86582
yash <yashsngh57@gmail.com>
2024-06-13 00:28:16 +0530

    Blog Dtos added

---
+++ src/main/java/com/drivingschool/Blog/Dtos/BlogRequestDTO.java
@@ -0,0 +1,25 @@
+package com.drivingschool.Blog.Dtos;
+
+public class BlogRequestDTO {
+    private String title;
+    private String content;
+    private Long userId;
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
--- src/main/java/com/drivingschool/User/UserController.java
+++ src/main/java/com/drivingschool/User/UserController.java
@@ -1,5 +1,4 @@
 package com.drivingschool.User;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.*;
 import com.drivingschool.User.Dto.UserRequestDTO;
 import com.drivingschool.User.Dto.UserResponseDTO;
 
